b8b3036af3c7acf8b4caaac64b18fac71e39fb52
socratone <gim2origin@gmail.com>
2020-05-12 01:30:54 +0900

    basic login

--- routes/signin.js
+++ routes/signin.js
@@ -1,4 +1,12 @@
-const signin = function(req, res) {
-  res.status(200).send('로그인이 요청됐습니다.');
+const { User } = require('../models/index');
+
+const signin = async function(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findAll({
+    where: { email, password }
+  });
+  if(user.length < 1) return res.status(404).send('이메일이나 암호가 틀렸습니다.')
+  const name = user[0].dataValues.name;
+  res.status(200).send(`${name}님 환영합니다.`);
 }
 module.exports = signin;
--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,6 @@
-const { User } = require('../models/index')
+const { User } = require('../models/index');
 
 const signup = async function(req, res) {
-  // console.log('what is the req.body', req.body)
   const { name, email, password } = req.body;
   await User.create({ name, email, password });
   res.status(200).send({ name, email, password });
