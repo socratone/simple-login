5aebdf98db3108a0400f85313b786d71a69a2d2b
socratone <gim2origin@gmail.com>
2020-05-12 21:31:25 +0900

    send cookie from session 3

--- routes/info.js
+++ routes/info.js
@@ -1,7 +1,7 @@
 const { User } = require('../models/index');
 
 const info = async function(req, res) {
-  if(req.session.name === 'receivedSessionId') {
+  if(req.session.key === 'temporaryReceivedSessionId') {
     res.send('사용자의 정보를 보여 드리겠습니다.')
   } else {
     res.send('사용자가 아닙니다.')
--- routes/signin.js
+++ routes/signin.js
@@ -7,8 +7,8 @@ const signin = async function(req, res) {
   });
 
   if(user.length < 1) return res.status(404).send('이메일이나 암호가 틀렸습니다.')
-  const name = 'receivedSessionId';
-  req.session.name = name;
-  res.status(200).send(`${name}님 환영합니다.`);
+  const key = 'temporaryReceivedSessionId'; // todo convert
+  req.session.key = key;
+  res.status(200).send(`${user[0].dataValues.name}님 환영합니다.`);
 }
 module.exports = signin;
