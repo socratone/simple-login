cb780f5af0fc03a3918cc1c183d4df1464a8b600
socratone <gim2origin@gmail.com>
2020-05-17 02:59:41 +0900

    token

--- routes/info.js
+++ routes/info.js
@@ -1,4 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 const info = function(req, res) {
-  res.status(200).send(req.cookies.data);
+  if(req.cookies.data) {
+    const token = req.cookies.data
+    const decoded = jwt.verify(token, 'secretKey');
+    res.status(200).send(`${decoded.name}님이 로그인 되어 있습니다.`);
+  } else {
+    res.status(200).send('로그인을 먼저 해야 합니다.')
+  }
 }
 module.exports = info; 
--- routes/signin.js
+++ routes/signin.js
@@ -1,4 +1,5 @@
 const { User } = require('../models/index');
+const jwt = require('jsonwebtoken');
 
 const signin = async function(req, res) {
   const { email, password } = req.body;
@@ -10,7 +11,8 @@ const signin = async function(req, res) {
     res.status(404).send('이메일이나 암호가 틀렸습니다.')
   } else {
     const data = user[0].dataValues;
-    res.cookie('data', data);
+    var token = jwt.sign({ name: data.name }, 'secretKey');
+    res.cookie('data', token);
     res.status(200).send(`${user[0].dataValues.name}님 환영합니다.`);
   }
 }
